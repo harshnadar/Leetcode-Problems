2eadee8ea02c21b3210b69de9a1794e0689b9855
Harsh Nadar <74041429+harshnadar@users.noreply.github.com>
2021-07-28 09:25:53 +0530

    Time: 180 ms (21.92%), Space: 94.2 MB (23.34%) - LeetHub

--- minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.cpp
+++ minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.cpp
@@ -4,16 +4,16 @@ public:
         int n=A.size();
         vector<int> swap(n+1,1), not_swap(n+1,0);
         for(int i=1;i<n;i++){
-            not_swap[i]=swap[i]=n;
+            swap[i]=not_swap[i]=n;
             if(A[i]>A[i-1] && B[i]>B[i-1]){
                 swap[i]=swap[i-1]+1;
                 not_swap[i]=not_swap[i-1];
             }
             if(A[i]>B[i-1] && A[i-1]<B[i]){
                 swap[i]=min(swap[i], not_swap[i-1]+1);
-                not_swap[i]=min(swap[i-1], not_swap[i]);
+                not_swap[i]=min(not_swap[i], swap[i-1]);
             }
         }
-        return min(swap[n-1], not_swap[n-1]);
+        return min(not_swap[n-1], swap[n-1]);
     }
 };
