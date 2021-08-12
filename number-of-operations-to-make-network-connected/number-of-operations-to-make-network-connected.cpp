bc5370b72942848247fa9fb4aed1cd9df6bd4839
Harsh Nadar <74041429+harshnadar@users.noreply.github.com>
2021-08-12 21:55:35 +0530

    Time: 291 ms (5.05%), Space: 44 MB (72.33%) - LeetHub

--- number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
-    vector<vector<int> > adj;
-    bool vis[(int)1e5+1];
+    int par[100002];
     
-    void dfs(int i){
-        vis[i]=true;
-        for(auto it: adj[i]){
-            if(!vis[it]){
-                dfs(it);
-            }
-        }
+    int find(int a){
+        if(par[a]==-1) return a;
+        return par[a]=find(par[a]);
+    }
+    
+    void union_set(int a, int b){
+        par[b]=a;
     }
     
     int makeConnected(int n, vector<vector<int>>& connections) {
-        if(n-1 > connections.size()) return -1;
-        adj.resize(n+1);
+        memset(par,-1,sizeof par);
+        // vector<pair<int, int> > ans;
+        int count=0;
         for(auto it: connections){
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
-        }
-        int cc=0;
-        for(int i=0;i<n;i++){
-            if(vis[i]==false){
-                dfs(i);
-                cc++;
+            int a = it[0];
+            int b=it[1];
+            int s1 = find(a);
+            int s2 = find(b);
+            if(s1!=s2){
+                union_set(s1,s2);
+            }
+            else{
+                count++;
+                // ans.push_back({a,b});
             }
         }
-        cout<<cc;
-        return cc-1;
+        set<int> s;
+        for(int i=0;i<n;i++){
+           s.insert(find(i));
+        }
+        if(s.size()-1<=count) return s.size()-1; 
+        return -1;
     }
 };
