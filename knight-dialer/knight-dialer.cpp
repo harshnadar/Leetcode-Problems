e2a0e7bf6da0cff0b74b2694dc3379e24f0051fa
Harsh Nadar <74041429+harshnadar@users.noreply.github.com>
2021-09-02 10:55:03 +0530

    Time: 218 ms (27.14%), Space: 6.7 MB (71.19%) - LeetHub

--- knight-dialer/knight-dialer.cpp
+++ knight-dialer/knight-dialer.cpp
@@ -1,32 +1,37 @@
 int dx[]={1,2,2,1,-1,-2,-2,-1};
 int dy[]={-2,-1,1,2,2,1,-1,-2};
+
 class Solution {
 public:
-    int dp[4][3][5001];
-    const int mod= 1e9+7;
+    int dp[5002][4][3];
+    int mod = 1e9+7;
     
     int solve(int i, int j, int n){
-        if(i<0 || i>=4 || j<0 || j>=3) return 0;
-        if(i==3 && (j==0 || j==2)) return 0;
-        if(n==1) return 1;
-        if(dp[i][j][n]>0) return dp[i][j][n];
-        long long int res=0;
+        if(n<0 || i<0 || i>=4 || j<0 || j>=3) return 0;
+        if(i==3&&(j==0 || j==2)) return 0;
+        if(dp[n][i][j]!=-1) return dp[n][i][j];
+        long long int temp=0;
         for(int k=0;k<8;k++){
-            res+=solve(i+dx[k], j+dy[k], n-1);
-            res%=mod;
+            temp+=solve(i+dx[k], j+dy[k], n-1);
+            temp%=mod;
         }
-        return dp[i][j][n]=res;
+        return dp[n][i][j]=temp;
     }
     
     int knightDialer(int n) {
-        // if(n==1) return 10;
-        long long int count=0;
+        memset(dp,-1,sizeof dp);
+        if(n==1) return 10;
+        for(int i=0;i<4;i++){
+            for(int j=0;j<3;j++)
+                if(!(i==3&&(j==0 || j==2))) dp[0][i][j]=1;
+        }
+        long long int x=0;
         for(int i=0;i<4;i++){
             for(int j=0;j<3;j++){
-                count+=solve(i,j,n);
-                count%=mod;
+                x+=solve(i,j,n-1);
+                x%=mod;
             }
         }
-        return (int)count;
+        return x;
     }
 };
