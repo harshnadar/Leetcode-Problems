49fdcad0a078aaf93c8c3fbfe0337d5c0e9722cd
Harsh Nadar <74041429+harshnadar@users.noreply.github.com>
2021-09-21 16:19:06 +0530

    Time: 56 ms (38.49%), Space: 27.2 MB (25.66%) - LeetHub

--- count-vowels-permutation/count-vowels-permutation.cpp
+++ count-vowels-permutation/count-vowels-permutation.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     int countVowelPermutation(int n) {
-        vector<vector<long long int> > dp(n+1,vector<long long int>(5,0));
-        for(int i=0;i<5;i++){
-            dp[1][i]=1;
-        }
-        int mod=1e9+7;
-        for(int i=2;i<=n;i++){
-            dp[i][0]=(dp[i-1][1]+dp[i-1][2]+dp[i-1][4])%mod;
-            
-            dp[i][1]= (dp[i-1][0]+ dp[i-1][2])%mod;
-            
-            dp[i][2]= (dp[i-1][1]+ dp[i-1][3])%mod;
-            
-            dp[i][3]= (dp[i-1][2])%mod;
-            
-            dp[i][4]= (dp[i-1][3] + dp[i-1][2])%mod;
-        }
-        
-        long long int res=0;
+        vector<vector<long long int> > dp(n, vector<long long int>(5,0));
+        int mod = 1e9+7;
         
         for(int i=0;i<5;i++){
-            res+=dp[n][i];
-            res%=mod;
+            dp[0][i]=1;
         }
         
-        return (int)res;
+        for(int i=1;i<n;i++){
+            dp[i][0] = (dp[i-1][1] + dp[i-1][2] + dp[i-1][4])%mod;
+            
+            dp[i][1] = (dp[i-1][2] + dp[i-1][0])%mod;
+            
+            dp[i][2] = (dp[i-1][1] + dp[i-1][3])%mod;
+            
+            dp[i][3] = (dp[i-1][2])%mod;
+            
+            dp[i][4] = (dp[i-1][3] + dp[i-1][2])%mod;
+        }
         
+        long long int sum=0;
+        
+        for(int i=0;i<5;i++){
+            sum+=dp[n-1][i];
+            sum%=mod;
+        }
+        
+        return sum;
     }
 };
