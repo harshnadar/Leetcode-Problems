60e47cdf317198fc61383cd9fde335532f26de8e
Harsh Nadar <74041429+harshnadar@users.noreply.github.com>
2021-09-23 18:04:25 +0530

    Time: 340 ms (87.88%), Space: 8.9 MB (86.28%) - LeetHub

--- burst-balloons/burst-balloons.cpp
+++ burst-balloons/burst-balloons.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
-    int maxCoins(vector<int>& a) {
-        int n=a.size();
-        if(n==0) return 0;
-        int dp[n+1][n+1];
-        memset(dp,0,sizeof dp);
-        for(int L=n-1;L>=0;L--){
-            for(int R=L;R<n;R++){
-                for(int i=L;i<=R;i++){
-                    dp[L][R]=max(dp[L][R], 
-                                 a[i]*(L?a[L-1]:1)*((R==n-1)?1:a[R+1]) + (L<=i-1?dp[L][i-1]:0)+ (R>=i+1?dp[i+1][R]:0));
-                }
-            }
+    int dp[503][503];
+    int n;
+    vector<int> num;
+    
+    int solve(int i, int j){
+        if(i>=j) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int res = 0;
+        for(int k=1+i;k<j;k++){
+            res = max(res, solve(i,k)+ num[i]*num[j]*num[k] + solve(k,j));
         }
-        return dp[0][n-1];
+        
+        return dp[i][j]=res;
+    }
+    
+    int maxCoins(vector<int>& nums) {
+        memset(dp,-1,sizeof dp);
+        
+        num.push_back(1);
+        
+        for(auto it: nums) num.push_back(it);
+        num.push_back(1);
+        
+        n=num.size();
+        
+        return solve(0,n-1);
     }
 };
