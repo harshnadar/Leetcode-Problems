5b321e23f14825142228716436ec0360bba9a43c
Harsh Nadar <74041429+harshnadar@users.noreply.github.com>
2022-06-17 08:22:01 +0530

    Time: 256 ms (97.08%), Space: 99.8 MB (62.56%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
+    int func(vector<int>& a, long long target, int mid){
+        return a[mid]>=target;
+    }
+    
+    int solve(vector<int>& a, long long target){
+        int n = a.size();
+        int lo = 0, hi = n;
+        while(lo<hi){
+            int mid = lo+(hi-lo)/2;
+            if(func(a,target,mid)) hi=mid;
+            else lo=mid+1;
+        }
+        
+        return lo;
+    }
     
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        int n = spells.size();
+        int m = potions.size();
+        
         sort(potions.begin(), potions.end());
-        vector<int> res;
-        for (int a: spells) {
-            long need = (success + a - 1) / a;
-            auto it = lower_bound(potions.begin(), potions.end(), need);
-            res.push_back(potions.end() - it);
+        vector<int> ans;
+        
+        for(int i=0;i<n;i++){
+            long long target = (success+spells[i]-1)/spells[i];
+            ans.push_back(m-solve(potions, target));
         }
-        return res;
+        return ans;
     }
 };
