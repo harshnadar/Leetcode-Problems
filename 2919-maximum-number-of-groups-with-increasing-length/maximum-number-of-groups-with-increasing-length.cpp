3f508bc1fd25f9c9d8d752b8ff1e877e046a6d72
Harsh Nadar <74041429+harshnadar@users.noreply.github.com>
2025-03-24 07:32:04 -0400

    Time: 63 ms (29.67%) | Memory: 129.6 MB (89.01%) - LeetSync

---
+++ 2919-maximum-number-of-groups-with-increasing-length/maximum-number-of-groups-with-increasing-length.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool satisfy(int groups, vector<int> &usageLimits) {
+        int gap = 0;
+        int h = groups;
+        for (int i = 0; i < usageLimits.size(); ++i) {
+            gap = max(h - usageLimits[i] + gap, 0);
+            if (h) h -= 1;
+        }
+        return gap == 0;
+    }
+    int maxIncreasingGroups(vector<int>& usageLimits) {
+        int n = usageLimits.size();
+        sort(usageLimits.begin(), usageLimits.end(), greater<int>());
+        int l = 1, r = n;
+        int res = 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (satisfy(mid, usageLimits)) {
+                res = max(res, mid);
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return res;
+        
+    }
+};
