f780e2ff05086d869f418285a1f6424c638ae253
Harsh Nadar <74041429+harshnadar@users.noreply.github.com>
2025-03-30 03:24:03 -0400

    Time: 11 ms (26.05%) | Memory: 22.1 MB (8.30%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
+    map<int, vector<int>> mp;
+    vector<bool> vis, dfs_vis;
+    bool cycleDetection(int node, vector<bool>& vis, vector<bool>& dfs_vis){
+        vis[node] = true;
+        dfs_vis[node] = true;
+
+        for(auto child: mp[node]){
+            if(!vis[child]){
+                if(cycleDetection(child, vis, dfs_vis)) return true;
+            }
+            else if(dfs_vis[child]) return true;
+        }
+        dfs_vis[node] = false;
+        return false;
+    }
     bool canFinish(int num, vector<vector<int>>& pre) {
+        vis.resize(num, 0);
+        dfs_vis.resize(num,0);
         
-        int n = pre.size();
-        map<int, vector<int> > mp;
-        int in[num+1];
-        memset(in,0,sizeof in);
+        // int in[num+1];
+        // memset(in,0,sizeof in);
         for(auto it: pre){
             mp[it[1]].push_back(it[0]);
-            in[it[0]]++;
         }
 
-        queue<int> q;
         for(int i=0;i<num;i++){
-            if(in[i]==0) q.push(i);
-        }
-        vector<int> res;
-        while(!q.empty()){
-            int curr_node = q.front();
-            q.pop();
-            res.push_back(curr_node);
-            for(auto child: mp[curr_node]){
-                in[child]--;
-                if(in[child] == 0) q.push(child);
+            if(!vis[i]){
+                if(cycleDetection(i, vis, dfs_vis)) return false;
             }
         }
 
-        return res.size() == num;
-
+        return true;
     }
 };
