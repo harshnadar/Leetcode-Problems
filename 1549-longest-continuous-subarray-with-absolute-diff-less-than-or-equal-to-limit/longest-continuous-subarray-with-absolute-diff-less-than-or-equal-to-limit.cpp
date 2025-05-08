f7bbfee00f8544182ff392afe48536c771b835d8
Harsh Nadar <74041429+harshnadar@users.noreply.github.com>
2025-05-08 04:02:47 -0400

    Time: 103 ms (45.80%) | Memory: 94 MB (41.48%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
         int n = nums.size();
-        int min_el = INT_MAX;
-        int max_el = INT_MIN;
         int ans=0;
         int i = 0, j=0;
         map<int, int> mp;
@@ -11,7 +9,6 @@ public:
             mp[nums[j]]++;
             int first_el = mp.begin()->first;
             int last_el = prev(mp.end())->first;
-            cout<<first_el<<" "<<last_el<<endl;
             while(i<j && last_el - first_el > limit){
                 mp[nums[i]]--;
                 if(mp[nums[i]] == 0) mp.erase(nums[i]);
